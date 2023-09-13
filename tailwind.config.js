e14bec48659c2c0135bc0ff6d14203be924ff374
AdrianoEscarabote <drian.escarabote@hotmail.com>
2023-09-13 16:11:45 -0300

    feat: adding colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,15 @@
 module.exports = {
   content: ["./src/**/*.{html,ts}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        red: "#FC4747",
+        darkBlue: "#10141E",
+        greyishBlue: "#5A698F",
+        semiDarkBlue: "#161D2F",
+        pureWhite: "#FFFFFF",
+      },
+    },
   },
   plugins: [],
 };
