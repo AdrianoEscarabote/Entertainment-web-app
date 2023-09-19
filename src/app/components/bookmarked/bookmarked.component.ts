59c858101855945ce0e54938687d7840737661fd
AdrianoEscarabote <drian.escarabote@hotmail.com>
2023-09-19 12:32:13 -0300

    feat: add bookmarked component

---
+++ src/app/components/bookmarked/bookmarked.component.ts
@@ -0,0 +1,7 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'bookmarked-component',
+  templateUrl: './bookmarked.component.html',
+})
+export class BookmarkedComponent {}
