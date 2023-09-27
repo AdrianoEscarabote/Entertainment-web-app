4cd17d2d367d11107931674556465e499bf26ea0
AdrianoEscarabote <drian.escarabote@hotmail.com>
2023-09-27 10:21:08 -0300

    feat: add app module and style.css

---
+++ frontend/src/app/app.module.ts
@@ -0,0 +1,56 @@
+// app.module.ts
+
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { StoreModule } from '@ngrx/store';
+import { HeaderComponent } from './components/header/header.component';
+import { ButtonComponent } from './components/button/button.component';
+import { LoginPage } from './pages/login/login.page';
+import { SignupPage } from './pages/signup/signup.page';
+import { HomePage } from './pages/home/home.page';
+import { InputComponent } from './components/input/input.component';
+import { MoviesComponent } from './components/movies/movies.component';
+import { SeriesComponent } from './components/series/series.component';
+import { BookmarkedComponent } from './components/bookmarked/bookmarked.component';
+import { HomeComponent } from './components/home/home.component';
+import { AuthLayoutComponent } from './components/auth-layout/auth-layout.component';
+import { TrendingComponent } from './components/trending/trending.component';
+import { RecommendedComponent } from './components/recommended/recommended.component';
+import { MovieInfoComponent } from './components/movieInfo/movieInfo.component';
+import { BookmarkButtonComponent } from './components/bookmarkButton/BookmarkButton.component';
+import { HttpClientModule } from '@angular/common/http';
+import { movieReducer } from './ngrx/movie.reducer';
+import { FormsModule } from '@angular/forms';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    ButtonComponent,
+    LoginPage,
+    SignupPage,
+    HomePage,
+    MoviesComponent,
+    SeriesComponent,
+    BookmarkedComponent,
+    HomeComponent,
+    InputComponent,
+    AuthLayoutComponent,
+    TrendingComponent,
+    RecommendedComponent,
+    MovieInfoComponent,
+    BookmarkButtonComponent,
+  ],
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    FormsModule,
+    AppRoutingModule,
+    StoreModule.forRoot({ movie: movieReducer }),
+  ],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
