d09d904cec929c0d324c2bef856e1ff7352a2411
AdrianoEscarabote <drian.escarabote@hotmail.com>
2023-10-03 13:32:33 -0300

    feat(components): add bookmark button

--- frontend/src/app/components/bookmarkButton/BookmarkButton.component.ts
+++ frontend/src/app/components/bookmarkButton/BookmarkButton.component.ts
@@ -1,7 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import axios from 'axios';
 
 @Component({
   selector: 'bookmarkbutton-component',
   templateUrl: './bookmarkButton.component.html',
 })
-export class BookmarkButtonComponent {}
+export class BookmarkButtonComponent {
+  @Input() showBookmarkedImg: boolean = false;
+  @Input() showTitle: string = '';
+
+  async bookmarkFn() {
+    const response = await axios
+      .post('https://real-erin-cow-boot.cyclic.app/bookmark/set', {
+        title: this.showTitle,
+      })
+      .then((res) => res.data);
+    this.showBookmarkedImg = response.ok;
+  }
+}
