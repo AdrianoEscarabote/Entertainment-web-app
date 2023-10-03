91209583e4ba5be373f07493faac5cda61380b02
AdrianoEscarabote <drian.escarabote@hotmail.com>
2023-10-03 13:36:13 -0300

    feat(components): add ngoninit to handle checktoken route

--- frontend/src/app/components/bookmarked/bookmarked.component.ts
+++ frontend/src/app/components/bookmarked/bookmarked.component.ts
@@ -1,7 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import axios from 'axios';
 
 @Component({
   selector: 'bookmarked-component',
   templateUrl: './bookmarked.component.html',
 })
-export class BookmarkedComponent {}
+export class BookmarkedComponent implements OnInit {
+  constructor(private router: Router) {}
+
+  ngOnInit(): void {
+    axios
+      .get('https://real-erin-cow-boot.cyclic.app/auth/checktoken')
+      .then((response) => {
+        if (response.status === 200) {
+          return;
+        }
+      })
+      .catch((error) => {
+        this.router.navigate(['/login']);
+      });
+  }
+}
--- frontend/src/app/components/home/home.component.ts
+++ frontend/src/app/components/home/home.component.ts
@@ -1,7 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import axios from 'axios';
 
 @Component({
   selector: 'home-component',
   templateUrl: 'home.component.html',
 })
-export class HomeComponent {}
+export class HomeComponent implements OnInit {
+  constructor(private router: Router) {}
+
+  ngOnInit(): void {
+    axios
+      .get('https://real-erin-cow-boot.cyclic.app/auth/checktoken')
+      .then((response) => {
+        if (response.status === 200) {
+          return;
+        }
+      })
+      .catch((error) => {
+        this.router.navigate(['/login']);
+      });
+  }
+}
