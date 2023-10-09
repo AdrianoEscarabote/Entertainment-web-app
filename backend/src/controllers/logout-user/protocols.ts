d0ec8012f00eb4480a3444cf61c3c017778d2a78
AdrianoEscarabote <drian.escarabote@hotmail.com>
2023-10-09 13:32:37 -0300

    feat(auth): move logout functionality to separate route and removed the LogoutUserController

--- backend/src/controllers/logout-user/protocols.ts
+++
@@ -1,3 +0,0 @@
-export interface LogoutUserReturn {
-  ok: boolean
-}
--- backend/src/routes/auth/logout.ts
+++ backend/src/routes/auth/logout.ts
@@ -1,20 +1,34 @@
-import { LogoutUserController } from "@/controllers/logout-user/logout-user"
+import { MongoClient } from "@/database/mongo"
 import checkToken from "@/middlewares/checktoken"
+import { MongoUser } from "@/repositories/mongo-protocols"
+import { clearCookies } from "@/utils/clearCookies"
 import * as express from "express"
+import { ObjectId } from "mongodb"
 
 const logoutRoute = express.Router()
 
 logoutRoute.post("/", checkToken, async (req, res) => {
-  const logoutuserController = new LogoutUserController()
+  try {
+    const id = req.cookies.id
 
-  const { body, statusCode } = await logoutuserController.handle(
-    {
-      body: req.body,
-    },
-    res,
-  )
+    const user = await MongoClient.db.collection<MongoUser>("users").findOne({
+      _id: new ObjectId(id),
+    })
 
-  res.status(statusCode).send(body)
+    if (!user) {
+      return res.status(400).json({
+        msg: "user is not present in the database!",
+      })
+    }
+
+    clearCookies(res)
+
+    res.status(200).send(true)
+  } catch (error) {
+    return res.status(500).json({
+      msg: "server error!",
+    })
+  }
 })
 
 export default logoutRoute
