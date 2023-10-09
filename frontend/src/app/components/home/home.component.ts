1517ad09730853e5e1117718934f61f785db7c15
AdrianoEscarabote <drian.escarabote@hotmail.com>
2023-10-09 16:03:20 -0300

    feat(components): add movieService to home component

--- frontend/src/app/components/home/home.component.ts
+++ frontend/src/app/components/home/home.component.ts
@@ -1,15 +1,17 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import axios from 'axios';
+import { MovieService } from 'src/app/service/movie.service';
 
 @Component({
   selector: 'home-component',
   templateUrl: 'home.component.html',
 })
 export class HomeComponent implements OnInit {
-  constructor(private router: Router) {}
+  constructor(private router: Router, private movieService: MovieService) {}
 
   ngOnInit(): void {
+    this.movieService.getMovies();
     axios
       .get('https://real-erin-cow-boot.cyclic.app/auth/checktoken', {
         withCredentials: true,
