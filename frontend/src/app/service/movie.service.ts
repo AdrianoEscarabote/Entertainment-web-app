ce84bd7684d4e01807d6fe851ad6e761ae8bdc3d
AdrianoEscarabote <drian.escarabote@hotmail.com>
2023-10-11 11:03:50 -0300

    chore(service): getting the values that are bookmarked

--- frontend/src/app/service/movie.service.ts
+++ frontend/src/app/service/movie.service.ts
@@ -7,12 +7,32 @@ import { loadMoviesSuccess } from '../ngrx/movie.actions';
   providedIn: 'root',
 })
 export class MovieService {
-  private movieDataUrl = '../../assets/data.json'; // Substitua pelo caminho correto
+  private movieDataUrl = '../../assets/data.json';
+  private getBookmarkedShowsUrl =
+    'https://real-erin-cow-boot.cyclic.app/bookmark/get';
   constructor(private http: HttpClient, private store: Store) {}
 
   getMovies(): void {
+    let bookmarkedShows: string[] = [];
+
+    this.http
+      .get<string[]>(this.getBookmarkedShowsUrl, {
+        withCredentials: true,
+      })
+      .subscribe((data) => {
+        bookmarkedShows = data;
+        console.log(bookmarkedShows);
+      });
+
     this.http.get<any[]>(this.movieDataUrl).subscribe((data) => {
-      this.store.dispatch(loadMoviesSuccess({ movies: data }));
+      const moviesWithBookmark = data.map((movie) => ({
+        ...movie,
+        isBookmarked: bookmarkedShows.includes(movie.title),
+      }));
+
+      console.log(moviesWithBookmark);
+
+      this.store.dispatch(loadMoviesSuccess({ movies: moviesWithBookmark }));
     });
   }
 }
