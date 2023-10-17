8245a749d57215c8bff1cd35633dac4815b2a522
AdrianoEscarabote <drian.escarabote@hotmail.com>
2023-10-17 10:26:22 -0300

    feat(header): highlight active navigation button

--- frontend/src/app/components/header/header.component.ts
+++ frontend/src/app/components/header/header.component.ts
@@ -1,14 +1,21 @@
-import { Component } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, NavigationEnd, Router } from '@angular/router';
+import { filter } from 'rxjs/operators';
 
 @Component({
   selector: 'header-component',
   templateUrl: './header.component.html',
 })
-export class HeaderComponent {
-  constructor(private route: ActivatedRoute) {}
+export class HeaderComponent implements OnInit {
+  activeRoute: string = '';
+  constructor(private router: Router, private activatedRoute: ActivatedRoute) {}
 
-  isRouteActive(routeName: string): boolean {
-    return this.route.snapshot.url[0]?.path === routeName;
+  ngOnInit(): void {
+    this.router.events
+      .pipe(filter((event) => event instanceof NavigationEnd))
+      .subscribe(() => {
+        this.activeRoute =
+          this.activatedRoute.snapshot.firstChild?.routeConfig?.path || '';
+      });
   }
 }
