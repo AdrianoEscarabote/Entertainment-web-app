04323ca6300dee1e6df196a7fb3f918fa9f4eeb1
AdrianoEscarabote <drian.escarabote@hotmail.com>
2023-10-19 16:04:15 -0300

    chore: changing getMovies

--- frontend/src/app/service/movie.service.ts
+++ frontend/src/app/service/movie.service.ts
@@ -15,29 +15,25 @@ export class MovieService {
   constructor(private http: HttpClient, private store: Store) {}
 
   async getMovies(): Promise<void> {
-    const bookmarkedShowsRequest = this.http.get<string[]>(
-      this.getBookmarkedShowsUrl,
-      {
-        withCredentials: true,
-      }
-    );
-    const moviesDataRequest = this.http.get<any[]>(this.movieDataUrl);
-
-    forkJoin([bookmarkedShowsRequest, moviesDataRequest])
-      .pipe(
-        mergeMap(([bookmarkedShows, moviesData]) => {
-          const moviesWithBookmark = moviesData.map((movie) => ({
-            ...movie,
-            isBookmarked: bookmarkedShows.includes(movie.title),
-          }));
-
-          // Dispatch the action and return it as an Observable
-          this.store.dispatch(
-            loadMoviesSuccess({ movies: moviesWithBookmark })
-          );
-          return of(null);
+    try {
+      const bookmarkedShows = await this.http
+        .get<string[]>(this.getBookmarkedShowsUrl, {
+          withCredentials: true,
         })
-      )
-      .subscribe();
+        .toPromise();
+
+      const moviesData = await this.http
+        .get<any[]>(this.movieDataUrl)
+        .toPromise();
+
+      const moviesWithBookmark = moviesData?.map((movie) => ({
+        ...movie,
+        isBookmarked: bookmarkedShows?.includes(movie.title),
+      }));
+
+      this.store.dispatch(loadMoviesSuccess({ movies: moviesWithBookmark! }));
+    } catch (error) {
+      console.error(error);
+    }
   }
 }
