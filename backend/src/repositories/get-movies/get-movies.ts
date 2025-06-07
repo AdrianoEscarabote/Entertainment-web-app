35c3cb68f6366fff39e828aea0741543d0e36844
AdrianoEscarabote <drian.escarabote@hotmail.com>
2025-06-07 00:00:03 -0300

    feat(repositories): add new methods getPopularMovies & getMoviesDetails

--- backend/src/repositories/get-movies/get-movies.ts
+++ backend/src/repositories/get-movies/get-movies.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import {
   GetMoviesParam,
   GetMoviesReturn,
@@ -6,22 +7,19 @@ import {
 
 export class GetMoviesRepository implements IGetMoviesRepository {
   async getPopularMovies(params: GetMoviesParam): Promise<GetMoviesReturn> {
-    const response = await fetch(
-      "https://api.themoviedb.org/3/trending/movie/day?language=en-US",
-      {
-        method: "GET",
-        headers: {
-          accept: "application/json",
-          Authorization: `${process.env.TMDB_API_KEY}`,
-        },
+    const response = await fetch("https://api.themoviedb.org/3/movie/popular", {
+      method: "GET",
+      headers: {
+        accept: "application/json",
+        Authorization: `${process.env.TMDB_API_KEY}`,
       },
-    )
+    })
       .then((response) => response.json())
       .then((data) => {
         return data
       })
       .catch((error) => {
-        console.error("Error fetching latest movie:", error)
+        console.error("Error fetching popular movie:", error)
       })
 
     if (response === undefined) {
@@ -34,7 +32,7 @@ export class GetMoviesRepository implements IGetMoviesRepository {
     }
   }
 
-  async getRecommendedMovies(params: GetMoviesParam): Promise<GetMoviesReturn> {
+  async getNowPlayingMovies(params: GetMoviesParam): Promise<GetMoviesReturn> {
     const response = await fetch(
       "https://api.themoviedb.org/3/movie/now_playing",
       {
@@ -50,17 +48,102 @@ export class GetMoviesRepository implements IGetMoviesRepository {
         return data
       })
       .catch((error) => {
-        console.error("Error fetching recommended movies:", error)
+        console.error("Error fetching now playing movies:", error)
       })
 
     if (response === undefined) {
-      throw new Error("Failed to fetch recommended movies")
+      throw new Error("Failed to fetch now playing movies")
     }
-
-    console.log(response)
 
     return {
       movies: response.results,
+      success: true,
+    }
+  }
+
+  async getTopRatedMovies(params: GetMoviesParam): Promise<GetMoviesReturn> {
+    const response = await fetch(
+      "https://api.themoviedb.org/3/movie/top_rated",
+      {
+        method: "GET",
+        headers: {
+          accept: "application/json",
+          Authorization: `${process.env.TMDB_API_KEY}`,
+        },
+      },
+    )
+      .then((response) => response.json())
+      .then((data) => {
+        return data
+      })
+      .catch((error) => {
+        console.error("Error fetching top rated movies:", error)
+      })
+
+    if (response === undefined) {
+      throw new Error("Failed to fetch top rated movies")
+    }
+
+    return {
+      movies: response.results,
+      success: true,
+    }
+  }
+
+  async getTrendingMovies(params: GetMoviesParam): Promise<GetMoviesReturn> {
+    const response = await fetch(
+      `https://api.themoviedb.org/3/trending/movie/week?language=en-US`,
+      {
+        method: "GET",
+        headers: {
+          accept: "application/json",
+          Authorization: `${process.env.TMDB_API_KEY}`,
+        },
+      },
+    )
+      .then((response) => response.json())
+      .then((data) => {
+        return data
+      })
+      .catch((error) => {
+        console.error("Error fetching trending movies:", error)
+      })
+
+    if (response === undefined) {
+      throw new Error("Failed to fetch trending movies")
+    }
+
+    return {
+      movies: response.results,
+      success: true,
+    }
+  }
+
+  async getMovieDetails(params: GetMoviesParam): Promise<GetMoviesReturn> {
+    const response = await fetch(
+      `https://api.themoviedb.org/3/movie/${params.movie_id}`,
+      {
+        method: "GET",
+        headers: {
+          accept: "application/json",
+          Authorization: `${process.env.TMDB_API_KEY}`,
+        },
+      },
+    )
+      .then((response) => response.json())
+      .then((data) => {
+        return data
+      })
+      .catch((error) => {
+        console.error("Error fetching movie details: ", error)
+      })
+
+    if (response === undefined) {
+      throw new Error("Failed to fetch movie details")
+    }
+
+    return {
+      movies: response,
       success: true,
     }
   }
