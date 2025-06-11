9880ea69df1ccef926ef2f7b67825e5a38dabd81
AdrianoEscarabote <drian.escarabote@hotmail.com>
2025-06-11 16:55:15 -0300

    chore(tailwind): add custom colors to tailwind.config.js

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -5,6 +5,7 @@ module.exports = {
     extend: {
       colors: {
         red: "#FC4747",
+        gray: "#9d9ea1",
         darkBlue: "#10141E",
         greyishBlue: "#5A698F",
         semiDarkBlue: "#161D2F",
