422fceeeb0d6b995991b17edc025060e74dfc14c
AdrianoEscarabote <drian.escarabote@hotmail.com>
2025-06-12 17:55:49 -0300

    feat(repositories): add getTvSeriesRepository

---
+++ backend/src/repositories/get-tv-series/get-tv-series.ts
@@ -0,0 +1,132 @@
+import {
+  GetTvSeriesParam,
+  IGetTvSeriesRepository,
+} from "@/controllers/get-tv-series/protocols"
+import { ShowType } from "@/models/Show"
+import axios from "axios"
+
+export class GetTvSeriesRepository implements IGetTvSeriesRepository {
+  async getTrendingTvSeries(params: GetTvSeriesParam): Promise<ShowType[]> {
+    const response = await axios
+      .get("https://api.themoviedb.org/3/trending/tv/week", {
+        headers: {
+          accept: "application/json",
+          Authorization: `${process.env.TMDB_API_KEY}`,
+        },
+      })
+      .then((response) => response.data)
+      .catch((error) => {
+        console.error("Error fetching trending TV series:", error)
+      })
+
+    if (response === undefined) {
+      throw new Error("Failed to fetch trending movies")
+    }
+
+    return response.results
+  }
+
+  async getPopularTvSeries(params: GetTvSeriesParam): Promise<ShowType[]> {
+    const response = await axios
+      .get("https://api.themoviedb.org/3/tv/popular", {
+        headers: {
+          accept: "application/json",
+          Authorization: `${process.env.TMDB_API_KEY}`,
+        },
+      })
+      .then((response) => response.data)
+      .catch((error) => {
+        console.error("Error fetching popular TV series:", error)
+      })
+
+    if (response === undefined) {
+      throw new Error("Failed to fetch popular movies")
+    }
+
+    return response.results
+  }
+
+  async getAiringTodayTvSeries(params: GetTvSeriesParam): Promise<ShowType[]> {
+    const response = await axios
+      .get("https://api.themoviedb.org/3/tv/airing_today", {
+        headers: {
+          accept: "application/json",
+          Authorization: `${process.env.TMDB_API_KEY}`,
+        },
+      })
+      .then((response) => response.data)
+      .catch((error) => {
+        console.error("Error fetching airing today TV series:", error)
+      })
+
+    if (response === undefined) {
+      throw new Error("Failed to fetch airing today movies")
+    }
+
+    return response.results
+  }
+
+  async getOnTheAirTvSeries(params: GetTvSeriesParam): Promise<ShowType[]> {
+    const response = await axios
+      .get("https://api.themoviedb.org/3/tv/on_the_air", {
+        headers: {
+          accept: "application/json",
+          Authorization: `${process.env.TMDB_API_KEY}`,
+        },
+      })
+      .then((response) => response.data)
+      .catch((error) => {
+        console.error("Error fetching on the air TV series:", error)
+      })
+
+    if (response === undefined) {
+      throw new Error("Failed to fetch on the air movies")
+    }
+
+    return response.results
+  }
+
+  async getTopRatedTvSeries(params: GetTvSeriesParam): Promise<ShowType[]> {
+    const response = await axios
+      .get("https://api.themoviedb.org/3/tv/top_rated", {
+        headers: {
+          accept: "application/json",
+          Authorization: `${process.env.TMDB_API_KEY}`,
+        },
+      })
+      .then((response) => response.data)
+      .catch((error) => {
+        console.error("Error fetching top rated TV series:", error)
+      })
+
+    if (response === undefined) {
+      throw new Error("Failed to fetch top rated movies")
+    }
+
+    return response.results
+  }
+
+  async getTvSeriesDetails(params: GetTvSeriesParam): Promise<ShowType> {
+    if (!params.tv_series_id) {
+      throw new Error("TV series ID is required")
+    }
+
+    const response = await axios
+      .get(`https://api.themoviedb.org/3/tv/${params.tv_series_id}`, {
+        headers: {
+          accept: "application/json",
+          Authorization: `${process.env.TMDB_API_KEY}`,
+        },
+      })
+      .then((response) => response.data)
+      .catch((error) => {
+        console.error("Error fetching TV series details:", error)
+      })
+
+    if (response === undefined) {
+      throw new Error("Failed to fetch TV series details")
+    }
+
+    return response
+  }
+}
