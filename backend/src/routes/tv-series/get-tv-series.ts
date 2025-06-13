13450b47262b63545655d3851a1002634dcfe9f1
AdrianoEscarabote <drian.escarabote@hotmail.com>
2025-06-13 17:10:08 -0300

    feat(routes): add tv series route

---
+++ backend/src/routes/tv-series/get-tv-series.ts
@@ -0,0 +1,29 @@
+import { GetTvSeriesController } from "@/controllers/get-tv-series/get-tv-series"
+import checkToken from "@/middlewares/checktoken"
+import { GetTvSeriesRepository } from "@/repositories/get-tv-series/get-tv-series"
+import * as express from "express"
+
+const getTvSeriesRoute = express.Router()
+
+getTvSeriesRoute.post("/", checkToken, async (req, res) => {
+  const bodyFormated = {
+    id: req.cookies.id,
+    token: req.cookies.token,
+    types: req.body.types,
+    ...req.body,
+  }
+
+  const getTvSeriesRepository = new GetTvSeriesRepository()
+  const getTvSeriesController = new GetTvSeriesController(getTvSeriesRepository)
+
+  const { body, statusCode } = await getTvSeriesController.handle(
+    {
+      body: bodyFormated,
+    },
+    res,
+  )
+
+  return res.status(statusCode).send(body)
+})
+
+export default getTvSeriesRoute
