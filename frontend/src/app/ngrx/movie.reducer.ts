10e43985df976ad46bfa3abcfe51451bcb69162a
AdrianoEscarabote <drian.escarabote@hotmail.com>
2025-06-13 12:26:03 -0300

    feat(ngrx): add & organize type movie actions by category

--- frontend/src/app/ngrx/movie.reducer.ts
+++ frontend/src/app/ngrx/movie.reducer.ts
@@ -1,67 +1,113 @@
 import { createReducer, on } from '@ngrx/store';
 import {
-  loadMoviesSuccess,
-  resetMovies,
-  setBookmarkedShow,
-  setSearchTerm,
+  loadMovieDetailsSuccess,
+  loadNowPlayingSuccess,
+  loadPopularSuccess,
+  loadTopRatedMovies,
+  loadTrendingSuccess,
+  loadUpcomingMovies,
 } from './movie.actions';
 
-interface Thumbnail {
-  small: string;
-  large: string;
-  medium: string;
+interface Genre {
+  id: number;
+  name: string;
 }
 
-interface Regular {
-  small: string;
-  medium: string;
-  large: string;
+interface ProductionCompany {
+  id: string;
+  logo_path: string;
+  name: string;
+  origin_country: string;
 }
-
-export interface MovieTypes {
+export interface MediaItem {
+  id: string;
   title: string;
+  tagline: string;
+  is_bookmarked: boolean;
+  rating: string;
+  release_date: string;
+  overview: string;
+  genres: Genre[];
+  imdb_id: string;
+  homepage: string;
+  runtime: string;
+  original_language: string;
+  status: string;
+  vote_average: number;
+  production_companies: ProductionCompany[];
+  media_type: 'movie' | 'tv';
+  backdrop_path: string;
+  poster_path: string;
   year: number;
   category: string;
-  rating: string;
-  isBookmarked: boolean;
-  isTrending: boolean;
-  thumbnail: {
-    trending: Thumbnail;
-    regular: Regular;
-  };
 }
 
-export interface MovieState {
-  movies: MovieTypes[];
+export interface AppState {
+  movies: {
+    popular: MediaItem[];
+    trending: MediaItem[];
+    nowPlaying: MediaItem[];
+    upcoming: MediaItem[];
+    topRated: MediaItem[];
+    movieDetails: MediaItem | null;
+  };
   searchTerm: string;
 }
 
-export const initialState: MovieState = {
-  movies: [],
+export const initialState: AppState = {
+  movies: {
+    popular: [],
+    trending: [],
+    nowPlaying: [],
+    upcoming: [],
+    topRated: [],
+    movieDetails: null,
+  },
   searchTerm: '',
 };
 
 export const movieReducer = createReducer(
   initialState,
-  on(loadMoviesSuccess, (state, { movies }) => ({
+  on(loadPopularSuccess, (state, { popular }) => ({
     ...state,
-    movies,
+    movies: {
+      ...state.movies,
+      popular: popular,
+    },
   })),
-  on(setSearchTerm, (state, { searchTerm }) => ({
+  on(loadTrendingSuccess, (state, { trending }) => ({
     ...state,
-    searchTerm,
+    movies: {
+      ...state.movies,
+      trending: trending,
+    },
   })),
-  on(resetMovies, () => initialState),
-  on(setBookmarkedShow, (state, { isBookmarked, title }) => ({
+  on(loadNowPlayingSuccess, (state, { nowPlaying }) => ({
     ...state,
-    movies: state.movies.map((movie) => {
-      if (movie.title === title) {
-        return {
-          ...movie,
-          isBookmarked,
-        };
-      }
-      return movie;
-    }),
+    movies: {
+      ...state.movies,
+      nowPlaying: nowPlaying,
+    },
+  })),
+  on(loadTopRatedMovies, (state, { topRated }) => ({
+    ...state,
+    movies: {
+      ...state.movies,
+      topRated: topRated,
+    },
+  })),
+  on(loadMovieDetailsSuccess, (state, { movieDetails }) => ({
+    ...state,
+    movies: {
+      ...state.movies,
+      movieDetails: movieDetails,
+    },
+  })),
+  on(loadUpcomingMovies, (state, { upcoming }) => ({
+    ...state,
+    movies: {
+      ...state.movies,
+      upcoming: upcoming,
+    },
   }))
 );
