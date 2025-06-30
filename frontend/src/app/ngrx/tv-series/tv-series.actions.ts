0466b4e2d14710a12a6bf91d2307e483b76c6e01
AdrianoEscarabote <drian.escarabote@hotmail.com>
2025-06-30 13:24:07 -0300

    refactor(ngrx/tv-series): standardize actions payload to include pagination and tvSeries list

--- frontend/src/app/ngrx/tv-series/tv-series.actions.ts
+++ frontend/src/app/ngrx/tv-series/tv-series.actions.ts
@@ -1,32 +1,70 @@
 import { createAction, props } from '@ngrx/store';
-import { MediaItem } from '../types';
+import { Genre, MediaItem } from '../types';
+
+export const loadAllTvSeriesSuccess = createAction(
+  '[TV Series API] Load All TV Series Success',
+  props<{
+    data: {
+      [category: string]: {
+        tvSeries: MediaItem[];
+        page: number;
+        total_pages: number;
+      };
+    };
+  }>()
+);
 
 export const loadTrendingTvSeries = createAction(
   '[TV Series] Load Trending Success',
-  props<{ trending: MediaItem[] }>()
+  props<{
+    trending: { page: number; total_pages: number; tvSeries: MediaItem[] };
+  }>()
 );
 
 export const loadPopularTvSeries = createAction(
   '[TV Series] Load Popular Success',
-  props<{ popular: MediaItem[] }>()
+  props<{
+    popular: { page: number; total_pages: number; tvSeries: MediaItem[] };
+  }>()
 );
 
 export const loadOnTheAirTvSeries = createAction(
   '[TV Series] Load On The Air Success',
-  props<{ onTheAir: MediaItem[] }>()
+  props<{
+    onTheAir: { page: number; total_pages: number; tvSeries: MediaItem[] };
+  }>()
 );
 
 export const loadTopRatedTvSeries = createAction(
   '[TV Series] Load Top Rated Success',
-  props<{ topRated: MediaItem[] }>()
+  props<{
+    topRated: { page: number; total_pages: number; tvSeries: MediaItem[] };
+  }>()
 );
 
 export const loadAiringTodayTvSeries = createAction(
   '[TV Series] Load Airing Today Success',
-  props<{ airingToday: MediaItem[] }>()
+  props<{
+    airingToday: { page: number; total_pages: number; tvSeries: MediaItem[] };
+  }>()
 );
 
 export const loadTvSeriesDetails = createAction(
   '[TV Series API] Load TV Series Details Success',
   props<{ tvSeriesDetails: MediaItem }>()
+);
+
+export const loadTvSeriesGenresList = createAction(
+  '[TV Series API] Load Genres List Success',
+  props<{ genresList: Genre[] }>()
+);
+
+export const loadTvSeriesByGenreSuccess = createAction(
+  '[TV Series] Load TV Series By Genre Success',
+  props<{
+    genre: string;
+    currentPage: number;
+    totalPages: number;
+    tvSeries: MediaItem[];
+  }>()
 );
