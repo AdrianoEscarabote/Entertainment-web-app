a49234d8c595adf86fa6be6e2a489e3e23459941
AdrianoEscarabote <drian.escarabote@hotmail.com>
2025-06-30 13:28:31 -0300

    refactor(ngrx): update selectors for new state structure with pagination

--- frontend/src/app/ngrx/tv-series/tv-series.selectors.ts
+++ frontend/src/app/ngrx/tv-series/tv-series.selectors.ts
@@ -1,34 +1,44 @@
 import { createFeatureSelector, createSelector } from '@ngrx/store';
-import { AppState, TvSeriesState } from '../types';
+import { TvSeriesState } from '../types';
 
 export const selectTvSeries = createFeatureSelector<TvSeriesState>('tvSeries');
 
 export const selectTrendingTvSeries = createSelector(
   selectTvSeries,
-  (state) => state.trending
+  (state) => state.trending.tvSeries
 );
 
 export const selectPopularTvSeries = createSelector(
   selectTvSeries,
-  (state) => state.popular
+  (state) => state.popular.tvSeries
 );
 
 export const selectOnTheAirTvSeries = createSelector(
   selectTvSeries,
-  (state) => state.onTheAir
+  (state) => state.onTheAir.tvSeries
 );
 
 export const selectTopRatedTvSeries = createSelector(
   selectTvSeries,
-  (state) => state.topRated
+  (state) => state.topRated.tvSeries
 );
 
 export const selectAiringTodayTvSeries = createSelector(
   selectTvSeries,
-  (state) => state.airingToday
+  (state) => state.airingToday.tvSeries
 );
 
 export const selectTvSeriesDetails = createSelector(
   selectTvSeries,
   (state) => state.tvSeriesDetails
+);
+
+export const selectTvSeriesGenresList = createSelector(
+  selectTvSeries,
+  (state) => state.genresList
+);
+
+export const selectTvSeriesByGenre = createSelector(
+  selectTvSeries,
+  (state) => state.tvSeriesByGenre
 );
