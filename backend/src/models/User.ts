23987138b3d4d9fd7c8f00ddd53b841785f73437
AdrianoEscarabote <drian.escarabote@hotmail.com>
2025-07-02 21:38:43 -0300

    chore(models): update UserTypes interface to include bookmarkedShows with movies & tvSeries

--- backend/src/models/User.ts
+++ backend/src/models/User.ts
@@ -2,5 +2,8 @@ export interface UserTypes {
   email: string
   password: string
   id: string
-  bookmarkedShows: string[]
+  bookmarkedShows: {
+    movies: string[]
+    tvSeries: string[]
+  }
 }
