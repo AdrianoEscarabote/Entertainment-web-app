2e0277b829fc15cd0d82677ba444ca5f17a9d116
AdrianoEscarabote <drian.escarabote@hotmail.com>
2025-07-08 11:40:18 -0300

    feat(components): improve movies & series components responsiveness and add unit tests

--- frontend/src/app/components/movies/movies.component.spec.ts
+++ frontend/src/app/components/movies/movies.component.spec.ts
@@ -1,6 +1,15 @@
-import { AppModule } from 'src/app/app.module';
-import { MoviesComponent } from './movies.component';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { MoviesComponent } from './movies.component';
+import { RouterTestingModule } from '@angular/router/testing';
+import { NO_ERRORS_SCHEMA } from '@angular/core';
+import { of } from 'rxjs';
+import { Store } from '@ngrx/store';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+
+const storeStub = {
+  select: () => of({ movies: [], genre: '', currentPage: 1, totalPages: 1 }),
+  dispatch: () => {},
+};
 
 describe('MoviesComponent', () => {
   let component: MoviesComponent;
@@ -9,16 +18,27 @@ describe('MoviesComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [MoviesComponent],
-      imports: [AppModule],
+      imports: [RouterTestingModule, HttpClientTestingModule],
+      schemas: [NO_ERRORS_SCHEMA],
+      providers: [{ provide: Store, useValue: storeStub }],
     }).compileComponents();
   });
 
   beforeEach(() => {
     fixture = TestBed.createComponent(MoviesComponent);
     component = fixture.componentInstance;
+    component.genres = of([{ id: 1, name: 'Action' }]);
+    component.basePath = 'movies';
+    fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it('should create the component', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should render genres as cards', () => {
+    const cardElements = fixture.nativeElement.querySelectorAll('.card');
+    expect(cardElements.length).toBe(2);
+    expect(cardElements[0].textContent.trim()).toBe('Action');
   });
 });
--- frontend/src/app/components/series/series.component.spec.ts
+++ frontend/src/app/components/series/series.component.spec.ts
@@ -1,6 +1,16 @@
 import { AppModule } from 'src/app/app.module';
 import { SeriesComponent } from './series.component';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { of } from 'rxjs';
+import { NO_ERRORS_SCHEMA } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { RouterTestingModule } from '@angular/router/testing';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+
+const storeStub = {
+  select: () => of({ movies: [], genre: '', currentPage: 1, totalPages: 1 }),
+  dispatch: () => {},
+};
 
 describe('SeriesComponent', () => {
   let component: SeriesComponent;
@@ -9,16 +19,26 @@ describe('SeriesComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [SeriesComponent],
-      imports: [AppModule],
+      imports: [RouterTestingModule, HttpClientTestingModule],
+      schemas: [NO_ERRORS_SCHEMA],
+      providers: [{ provide: Store, useValue: storeStub }],
     }).compileComponents();
   });
 
   beforeEach(() => {
     fixture = TestBed.createComponent(SeriesComponent);
     component = fixture.componentInstance;
+    component.genres = of([{ id: 1, name: 'Action & Adventure' }]);
+    fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should render genres as cards', () => {
+    const cardElements = fixture.nativeElement.querySelectorAll('.card');
+    expect(cardElements.length).toBe(2);
+    expect(cardElements[0].textContent.trim()).toBe('Action & Adventure');
   });
 });
