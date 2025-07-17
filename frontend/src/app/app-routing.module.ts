8c199e1f0356118f1f38b3ceb3875e6939d54fd6
AdrianoEscarabote <drian.escarabote@hotmail.com>
2025-07-17 12:08:51 -0300

    refactor(routing): update and reorder routes for main pages and genres

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -4,25 +4,13 @@ import { LoginPage } from './pages/login/login.page';
 import { SignupPage } from './pages/signup/signup.page';
 import { MoviesComponent } from './components/movies/movies.component';
 import { SeriesComponent } from './components/series/series.component';
-import { HomeComponent } from './components/home/home.component';
-import { MoviePage } from './pages/movie/movie.page';
-import { TvPage } from './pages/tv/tv.page';
 import { MediaGenrePage } from './pages/media-genre-page/media-genre.page';
+import { MediaCategoryListComponent } from './components/media-category-list/media-category-list.component';
+import { BookmarkedPage } from './pages/bookmarked/bookmarked.page';
+import { HomePage } from './pages/home/home.page';
+import { MediaDetailsPage } from './pages/media-details-page/media-details.page';
 
 const routes: Routes = [
-  { path: 'home', component: HomeComponent },
-  { path: 'movies', component: MoviesComponent },
-  { path: 'tv-series', component: SeriesComponent },
-  {
-    path: 'movies/genre/:genre-name/:genre-id/:page',
-    component: MediaGenrePage,
-  },
-  {
-    path: 'tv-series/genre/:genre-name/:genre-id/:page',
-    component: MediaGenrePage,
-  },
-  { path: 'movie/:id', component: MoviePage },
-  { path: 'tv/:id', component: TvPage },
   {
     path: 'login',
     component: LoginPage,
@@ -33,6 +21,21 @@ const routes: Routes = [
     component: SignupPage,
     data: { layout: 'auth' },
   },
+  { path: 'home', component: HomePage },
+  { path: 'movies', component: MoviesComponent },
+  { path: 'tv-series', component: SeriesComponent },
+  { path: 'bookmarked', component: BookmarkedPage },
+  { path: 'movies/:category/:page', component: MediaCategoryListComponent },
+  { path: 'tv-series/:category/:page', component: MediaCategoryListComponent },
+  {
+    path: 'movies/genre/:genre-name/:genre-id/:page',
+    component: MediaGenrePage,
+  },
+  {
+    path: 'tv-series/genre/:genre-name/:genre-id/:page',
+    component: MediaGenrePage,
+  },
+  { path: 'media/:type/:id', component: MediaDetailsPage },
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: '**', redirectTo: '/home' },
 ];
