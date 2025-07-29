11c1acfa206d2eb9d321b30c207c75aa43002a40
AdrianoEscarabote <drian.escarabote@hotmail.com>
2025-07-29 17:32:32 -0300

    chore(environment): update API URL and TMDB API key for development

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,4 +1,6 @@
 export const environment = {
   production: false,
   apiUrl: 'https://entertainment-web-app-powk.onrender.com',
+  tmdbApiKey:
+    'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIxNDgxZWUzYTY4NjZhYWQ5Yzc3MGJmYWQxODMzNjlmNiIsIm5iZiI6MTY5NTEzNjk4NC43MTIsInN1YiI6IjY1MDliY2Q4OGE4OGIyMDBhZDVlMWE0MSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.U0ZyllWOTNmGy1bNyMKsuBNmTF3aR39T60FodMKQn9I',
 };
