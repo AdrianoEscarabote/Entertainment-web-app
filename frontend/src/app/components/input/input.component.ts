fcc7396a930ed4d72081d828855d68eaeeb581fb
AdrianoEscarabote <drian.escarabote@hotmail.com>
2025-07-29 17:06:03 -0300

    chore(components/input): add goToPage method & alt text to img

--- frontend/src/app/components/input/input.component.ts
+++ frontend/src/app/components/input/input.component.ts
@@ -15,25 +15,37 @@ export class InputComponent {
   @Input() searchTerm: string = '';
   @Output() searchTermChange = new EventEmitter<string>();
   @Output() search = new EventEmitter<string>();
+  @Output() searchPageChange = new EventEmitter<number>();
 
   error: string = '';
 
   constructor(private store: Store, private router: Router) {}
 
-  get mediaType(): 'movie' | 'tv' {
+  get mediaType(): 'movie' | 'tv' | 'multi' {
     if (this.router.url.startsWith('/movies')) {
       return 'movie';
     }
     if (this.router.url.startsWith('/tv-series')) {
       return 'tv';
     }
-    return 'movie';
+    return 'multi';
   }
 
   onInputChange() {
     this.error = '';
     this.searchTermChange.emit(this.searchTerm);
     this.store.dispatch(setSearchTerm({ searchTerm: this.searchTerm }));
+  }
+
+  goToPage(page: number) {
+    this.searchPageChange.emit(page);
+    this.store.dispatch(
+      loadSearchResults({
+        searchTerm: this.searchTerm,
+        mediaType: this.mediaType,
+        page,
+      })
+    );
   }
 
   onSearch() {
